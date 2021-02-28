f8925c28a00cbe5c0cbde2cabe4027f2203921cf
Adrian <adrianu161999@gmail.com>
2021-02-28 18:51:23 +0100

    Add .Xresources, .xinitrc, xmonad and xmobar config files

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,43 @@
+import System.IO
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.Paste
+import XMonad.Config.Desktop
+import Graphics.X11.ExtraTypes.XF86
+
+main = do 
+	xmproc <- spawnPipe "xmobar"
+	xmonad $ ewmh $ docks def
+		{ terminal	= myTerminal
+		, modMask	= myModMask
+		, borderWidth 	= myBorderWidth
+		, workspaces	= myWorkspaces
+
+		, focusedBorderColor = "#ff6680"
+
+		-- docks management
+		, layoutHook	= avoidStruts $ layoutHook defaultConfig
+		, manageHook	= manageHook def <+> manageDocks
+		, logHook 	= dynamicLogWithPP xmobarPP
+			{ ppOutput	 = hPutStrLn xmproc
+			, ppTitle	 = xmobarColor "#69e1d3" "" . shorten 50
+			}
+
+		-- fullscreen windows handling
+		, handleEventHook = ewmhDesktopsEventHook <+> fullscreenEventHook
+
+		} `additionalKeys`
+		[ ((myModMask, xK_t), spawn "brave") -- launch web browser
+		, ((0, xK_Insert), pasteSelection) -- X-selection-paste buffer
+		, ((0, xF86XK_AudioRaiseVolume), spawn "amixer set Master 5%+")
+		, ((0, xF86XK_AudioLowerVolume), spawn "amixer set Master 5%-")
+		]
+
+myTerminal = "urxvt"
+myModMask = mod4Mask
+myBorderWidth = 3
+myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
